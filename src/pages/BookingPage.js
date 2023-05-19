9213238056062613881b5c8de80e551241c06d18
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-05-19 16:09:54 +0900

    booking page is created

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const BookingPage = () => {
+    return <>
+    </>
+}
+
+export default BookingPage;
---
+++ src/pages/MyBookingPage.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const MyBookingPage = () => {
+    return <>
+    </>
+}
+
+export default MyBookingPage;
