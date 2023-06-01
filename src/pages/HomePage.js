99f1be090453388b5e2f2a6f3cc39e88512fc03f
healim01 <22000770@handong.edu>
2023-06-02 04:37:14 +0900

    feat: add depart & arriv part

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,9 @@ import {React} from "react";
 import { Button } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 
+import Header from "../components/Header";
+
+
 function HomePage() {
     const isDesktopOrMobile = useMediaQuery({ query: '(max-width:768px)' });
     const HomePageComponent = styled.div`
@@ -20,45 +23,55 @@ function HomePage() {
     `;
 
     const ContentContainer = styled.div`
-        background-color: rgba(255, 255, 255, 0.6);
+        background-color: rgba(255, 255, 255, 0.8);
         padding: 20px;
         text-align: center;
     `;
 
     const navigate = useNavigate();
- 
-    const navigateToPurchase = () => {
-        navigate("/Booking");
+
+    // const navigateToPurchase = () => {
+    //     navigate("/Booking");
+    // };
+    const navigateToDeparture = () => {
+        navigate("/DepartBooking");
+    };
+    const navigateToArrival = () => {
+        navigate("/ArrivBooking");
     };
 
     
     return (
-        <HomePageComponent>
-            {isDesktopOrMobile !== true ? (
-                <div>
-                    <ContentContainer>
-                        <h1>당신의 여행을 위해 콜밴을 예약하세요!</h1>
-                        <p>AirportHelp Center Int'l is a new transportation company,</p> 
-                        <p>specializing 'door to door' service, particularly for the passengers arriving/departing at Incheon Int'l Airport.</p>
-                        <br/>
-                        <Button variant="outlined" onClick={navigateToPurchase} style={{ margin: '5px' }}>출발 예약하기</Button>
-                        <Button variant="outlined" onClick={navigateToPurchase} style={{ margin: '5px' }}>도착 예약하기</Button>
-                        <Button variant="outlined" style={{ margin: '5px' }}>예약 확인하기</Button>
-                    </ContentContainer>
-                </div>
-            ) : (
-                <div>
-                    <ContentContainer>
-                        <h1>당신의 여행을 위해 콜밴을 예약하세요!</h1>
-                        <p>AirportHelp Center Int'l is a new transportation company, specializing 'door to door' service, particularly for the passengers arriving/departing at Incheon Int'l Airport.</p>
-                            <Button variant="outlined" onClick={navigateToPurchase} style={{ margin: '2px' }}>출발 예약하기</Button>
-                            <Button variant="outlined" onClick={navigateToPurchase} style={{ margin: '2px' }}>도착 예약하기</Button>
-                            <Button variant="outlined" style={{ margin: '2px' }}>예약 확인하기</Button>
-              
-                    </ContentContainer>
-                </div>
-            )}
-        </HomePageComponent>
+        <>
+            <Header/>
+            <></>
+            <HomePageComponent>
+                {isDesktopOrMobile !== true ? (
+                    <div>
+                        <ContentContainer>
+                            <h1>당신의 여행을 위해 콜밴을 예약하세요!</h1>
+                            <p>AirportHelp Center Int'l is a new transportation company,</p> 
+                            <p>specializing 'door to door' service, particularly for the passengers arriving/departing at Incheon Int'l Airport.</p>
+                            <br/>
+                            <Button variant="outlined" onClick={navigateToDeparture} style={{ margin: '5px' }}>출발 예약하기</Button>
+                            <Button variant="outlined" onClick={navigateToArrival} style={{ margin: '5px' }}>도착 예약하기</Button>
+                            <Button variant="outlined" style={{ margin: '5px' }}>예약 확인하기</Button>
+                        </ContentContainer>
+                    </div>
+                ) : (
+                    <div>
+                        <ContentContainer>
+                            <h1>당신의 여행을 위해 콜밴을 예약하세요!</h1>
+                            <p>AirportHelp Center Int'l is a new transportation company, specializing 'door to door' service, particularly for the passengers arriving/departing at Incheon Int'l Airport.</p>
+                            <Button variant="outlined" onClick={navigateToDeparture} style={{ margin: '2px' }}>출발 예약하기</Button>
+                                <Button variant="outlined" onClick={navigateToArrival} style={{ margin: '2px' }}>도착 예약하기</Button>
+                                <Button variant="outlined" style={{ margin: '2px' }}>예약 확인하기</Button>
+                
+                        </ContentContainer>
+                    </div>
+                )}
+            </HomePageComponent>
+        </>
     );
 }
 
